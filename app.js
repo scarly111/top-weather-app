f9f8f5e1117da26b3b01aef4c57a92470ce05fda
ScarLi <yigitcagri48@gmail.com>
2025-04-21 14:09:59 +0300

    fetch weather data from api

--- app.js
+++ app.js
@@ -11,3 +11,22 @@
 require('dotenv').config()
 
 const apiKey = process.env.API_KEY
+
+
+const getData = async () => {
+    try {
+
+        const location = "New York"; // replace or make dynamic
+        const date1 = "2024-04-01";
+        const date2 = "2024-04-07";
+
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}/${date2}?key=${apiKey}`)
+        const data = await response.json()
+        console.log(data)
+        
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+getData()
